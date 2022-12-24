bf98945aae60163175affd4ac083696fa9249f71
selmanbaskaya <selmanbaskaya1@gmail.com>
2022-12-24 23:45:53 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<script setup>
+import { ref } from "vue";
+import TreeView from "@/components/TreeView.vue";
+
+const root = ref({
+  label: "Level 1",
+  nodes: [
+    {
+      label: "Level 1.1",
+      nodes: [
+        {
+          label: "Level 1.1.1"
+        }
+      ]
+    },
+    {
+      label: "Level 1.2",
+      nodes: [
+        {
+          label: "Level 1.2.1",
+          nodes: [
+            {
+              label: "Level 1.2.1.1"
+            },
+            {
+              label: "Level 1.2.1.2",
+              nodes: [
+                {
+                  label: "Level 1.2.1.2.1"
+                }   
+              ]
+            }
+          ]
+        },
+        {
+          label: "Level 1.2.2",
+          nodes: [
+            {
+              label: "Level 1.2.2.1"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      label: "Level 1.3",
+      nodes: [
+        {
+          label: "Level 1.3.1"
+        },
+        {
+          label: "Level 1.3.2"
+        }
+      ]
+    }
+  ]
+});
+</script>
+
+<template>
+  <TreeView
+    :label="root.label"
+    :nodes="root.nodes" 
+  />
+</template>
+
+<style scoped></style>
---
+++ src/components/TreeView.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue"
+import TreeView from "@/components/TreeView.vue";
+
+const props = defineProps(['label', 'nodes'])
+
+const childVisibility = ref(false)
+</script>
+
+<template>
+    <div
+      class="label"
+      @click="childVisibility = !childVisibility"
+    >
+        {{ label }}
+    </div>
+    <div
+      class="child"
+      v-if="childVisibility"
+    >
+        <TreeView
+          v-for="child in nodes"
+          :label="child.label"
+          :nodes="child.nodes"
+        />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.label {
+    margin: 5px 10px;
+    color: grey-5;
+}
+
+.child {
+    margin-left: 20px;
+}
+</style>
